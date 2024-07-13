67aaafe2d35aba644beb7792c28f6233d14935c5
Onur Aydinoglu <onur.aydinoglu.98@gmail.com>
2024-07-13 17:24:45 +0300

    feat: add footer to the project

--- obj/Debug/net7.0/Basic.AssemblyInfo.cs
+++ obj/Debug/net7.0/Basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736474e90a81bb12cc3c8688be351c48da75db47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
